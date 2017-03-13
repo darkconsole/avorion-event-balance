f1f2f2d15403203fa0fecd2f42ccf679f8193d89
darkconsole <darkcee.legit@gmail.com>
2017-03-12 22:55:18 -0500

    tool for my lazy ass

---
+++ push.php
@@ -0,0 +1,52 @@
+<?php
+
+// this script will copy the files to our server via my remote ssh mount.
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+define('ProjectRoot','..');
+define('StockDir', '/avorion-0.10.2/data/scripts');
+define('ModDir', '/avorion-event-balance/data/scripts');
+define('PatchDir', '/avorion-event-balance/patches');
+define('RemoteDir','z:\home\a1270\steam_games\avorion\data\scripts');
+
+define('Files',[
+	'/lib/dcc-event-balance/main.lua' => '/patch-lib-dcc-event-balance-main.diff',
+	'/events/pirateattack.lua'        => '/patch-events-pirateattack.diff',
+	'/player/eventscheduler.lua'      => '/patch-player-eventscheduler.diff'
+]);
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+function
+Pathify(String $Filepath):
+String {
+/*//
+generate proper file paths for the os given that we are writing the code for
+forward slashes in mind. seems to be needed for some windows commands.
+//*/
+
+	$Filepath = str_replace('%VERSION%','Version',$Filepath);
+
+	return str_replace('/',DIRECTORY_SEPARATOR,$Filepath);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+$File;
+$Patch;
+$Command;
+
+foreach(Files as $File => $Patch) {
+	$Command = sprintf(
+		'xcopy /R /Y %s %s',
+		escapeshellarg(Pathify(ProjectRoot.ModDir.$File)),
+		escapeshellarg(Pathify(RemoteDir.$File))
+	);
+
+	echo $Command, PHP_EOL;
+	system($Command);
+}
